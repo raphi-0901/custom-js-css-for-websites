c1ec3547af4267fb0deee62fd58efdf94ebbe41d
Raphael Wirnsberger <rwirnsberger@anexia-it.com>
2025-09-29 15:10:48 +0200

    update pm codes

--- intranet.anexia-it.com/highligh-pm.js
+++ intranet.anexia-it.com/highligh-pm.js
@@ -1,43 +1,46 @@
 function highlightPMsModule() {
-    const config = {
-        developers: ['TR', 'LT', 'JK', 'LK', 'RAW', 'DS'],
-        marketing: [
-            'BK',
-            'CL',
-            'SHO',
-            'TW',
-            'BAL',
-            'KK',
-            'CAS',
-            'DAR',
-            'BD',
-            'AFE',
-        ],
-    };
+	const config = {
+		developers: ['TR', 'LT', 'JK', 'LK', 'RAW', 'DS', 'BEK'],
+		marketing: [
+			'BK',
+			'CL',
+			'SHO',
+			'TW',
+			'BAL',
+			'KK',
+			'CAS',
+			'DAR',
+			'BD',
+			'AFE',
+			'SBE',
+			'JGA',
+		],
+	};
 
-    function highlightPMs() {
-        document
-            .querySelectorAll('.content .table-label-anx-private')
-            .forEach((val) => {
-                const text = val.textContent.trim();
+	function highlightPMs() {
+		document
+			.querySelectorAll('.content .table-label-anx-private')
+			.forEach((val) => {
+				const text = val.textContent.trim();
 
-                if (config.developers.includes(text)) {
-                    val.style.color = '#640000';
-                    val.style.borderLeft = '8px solid #FF0000';
-                }
+				if (config.developers.includes(text)) {
+					val.style.color = '#640000';
+					val.style.borderLeft = '8px solid #FF0000';
+				}
 
-                if (config.marketing.includes(text)) {
-                    val.style.color = '#644000';
-                    val.style.borderLeft = '8px solid #FFA500';
-                }
-            });
-    }
+				if (config.marketing.includes(text)) {
+					val.style.color = '#644000';
+					val.style.borderLeft = '8px solid #FFA500';
+				}
+			});
+	}
 
-    try {
-        highlightPMs();
-    } catch (e) {
-        console.error('Error in highlightPMs:', e);
-    }
+	try {
+		highlightPMs();
+	} catch (e) {
+		console.error('Error in highlightPMs:', e);
+	}
 }
 
 highlightPMsModule();
+
